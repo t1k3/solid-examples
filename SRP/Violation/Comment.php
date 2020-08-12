dd50b5081a8e464756c03117b31bdcd4600e1af4
T1k3 <t1k3hu@gmail.com>
2020-08-12 09:33:02 +0200

    Add SRP violation

---
+++ SRP/Violation/Comment.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace T1k3\SolidExamples\SRP\Violation;
+
+use DateTime;
+use DateTimeInterface;
+
+class Comment
+{
+    private string $comment;
+
+    private DateTimeInterface $createdAt;
+
+    public function __construct(string $comment)
+    {
+        $this->comment = $comment;
+        $this->createdAt = new DateTime();
+    }
+
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function getContents(): array
+    {
+        return [
+            "comment" => $this->getComment(),
+            "createdAt" => $this->getCreatedAt(),
+        ];
+    }
+
+    public function formatJson(): string
+    {
+        return json_encode($this->getContents());
+    }
+}
